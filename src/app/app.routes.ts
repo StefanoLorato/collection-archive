13ff838c41fa2115093c50d38e4248132a4e3144
martapetruzzelli <martapetruzzelli1@gmail.com>
2025-06-07 14:51:58 +0200

    salvataggio dopo merge da ste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { CollectionListComponent } from './components/collection/collection-lists/collection-list.component';
 import { CollectionFormComponent } from './components/collection/collection-form/collection-form.component';
+import { ItemListComponent } from './components/item/item-list/item-list.component';
 
 export const routes: Routes = [
      { path: '', redirectTo: 'collection-list', pathMatch: 'full' },
      { path: 'home', component: HomeComponent },
      { path: 'collection-list', component: CollectionListComponent },
      { path: 'collection-form', component: CollectionFormComponent },
+     { path: 'item-list', component: ItemListComponent},
+
 ];
--- src/app/components/item/item-list/item-list.component.ts
+++ src/app/components/item/item-list/item-list.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Item } from '../../../models/item';
+import { ItemService } from '../../../service/itemService';
+import { Observable } from 'rxjs';
+import { ItemCardComponent } from '../item-card/item-card.component';
 
 @Component({
   selector: 'app-item-list',
-  imports: [],
-  templateUrl: './item-list.component.html',
+  imports: [ItemCardComponent],
+  templateUrl: './item-list.html',
   styleUrl: './item-list.component.css'
 })
-export class ItemListComponent {
+export class ItemListComponent  implements OnInit{
+  item!: Item;
+  list: Item[] = [];
+  private _service = inject(ItemService);
+
+  ngOnInit(): void {
+    this.loadItem();
+  }
+
+  loadItem() {
+    const itemObservable: Observable<Item[]> = this._service.getItems();
+    itemObservable.subscribe({
+      next: items => this.list = items,
+      error: e => alert("Errore nel caricamento dell'item " + e)
+    });
+  }
+
+  findItemById(id:number) {
+    this._service.getItemById(id).subscribe({
+      next: t => this.item = t,
+      error: e => alert("Errore nella ricerca dell'item")
+    });
+  }
 
 }
