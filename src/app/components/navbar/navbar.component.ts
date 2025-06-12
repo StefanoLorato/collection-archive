286cdda50b4c21ef8c05cd04f8d973dfec19eab6
MarcelloCoppolino <marcello95c@gmail.com>
2025-06-13 00:05:07 +0200

    aggiungo funzione di ricerca per nome delle collection

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,14 +5,18 @@ import { User } from '../../models/user';
 import { AuthService } from '../../service/authService';
 import { CategoryService } from '../../service/categoryService';
 import { Category } from '../../models/category';
+import { CommonModule, ɵnormalizeQueryParams } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  searchTerm: String = '';
   private _dataService = inject(DataService);
   currentUser: User | null = null;
   private _authService = inject(AuthService);
@@ -38,5 +42,9 @@ export class NavbarComponent {
     this._authService.logout();
     alert("You succesfully logged out. Thank you for your visit");
     this._router.navigate(['/login']);
+  }
+
+  search(){
+    this._router.navigate(['/collection-list'], {queryParams: {collectionName : this.searchTerm}});
   }
 }
