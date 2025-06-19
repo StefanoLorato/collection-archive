ac598e53bba237c498977a84085f4192b8634950
martapetruzzelli <martapetruzzelli1@gmail.com>
2025-06-19 13:34:19 +0200

    sistema css chat

--- src/app/components/discussion/message/message.component.ts
+++ src/app/components/discussion/message/message.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { Message } from '../../../models/message';
+import { DataService } from '../../../service/dataService';
+import { User } from '../../../models/user';
 
 @Component({
   selector: 'app-message',
@@ -7,6 +9,16 @@ import { Message } from '../../../models/message';
   templateUrl: './message.component.html',
   styleUrl: './message.component.css'
 })
-export class MessageComponent {
-@Input("message") message!:Message;
+export class MessageComponent implements OnInit{
+  @Input("message") message!:Message;
+  private _dataService = inject(DataService);
+  currentUser!:User;
+
+  ngOnInit(): void {
+    this._dataService.selectedUserObservable.subscribe(user => {
+      if(user != null){
+        this.currentUser = user;
+      }
+    });
+  }
 }
